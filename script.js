46f67bcddc4a0ca9b33861f43aa2e5ef2a6693fb
fajarnursodiqq <fajarnursodiqq@gmail.com>
2023-06-23 02:25:42 +0700

    week 2 day 5 membuat burger item

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function saySubscribe() {
+/*function saySubscribe() {
   alert("TERIMAKASIH SUDAH MAU SUBSCRIBE");
-}
+}*/
+
+const selectElement = (element) => document.querySelector(element);
+selectElement(".mobile-menu").addEventListener("click", () => {
+  selectElement("header").classList.toggle("active");
+});
