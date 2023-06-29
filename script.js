2fa611445a85798ec332c73ccbbc04a8b12c3e20
fajarnursodiqq <fajarnursodiqq@gmail.com>
2023-06-29 14:26:49 +0700

    week 3 day 4 membuat layout mobile

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/*function saySubscribe() {
-  alert("TERIMAKASIH SUDAH MAU SUBSCRIBE");
-}*/
-
 const selectElement = (element) => document.querySelector(element);
 selectElement(".mobile-menu").addEventListener("click", () => {
   selectElement("header").classList.toggle("active");
